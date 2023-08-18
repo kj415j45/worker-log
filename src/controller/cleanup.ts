a15a39d8e8c0e8ac2da72c3944681213711cf4c8
kj415j45 <919815238@qq.com>
2023-08-18 16:46:49 +0800

    Add missing binding.

--- src/controller/cleanup.ts
+++ src/controller/cleanup.ts
@@ -1,4 +1,4 @@
 export default async function exist(env: Env) {
 	const expiry = new Date().setDate(new Date().getDate() - 1);
-	return env.DB.prepare('DELETE FROM log WHERE endpoint IN ( SELECT DISTINCT endpoint FROM log WHERE timestamp < ? );');
+	return env.DB.prepare('DELETE FROM log WHERE endpoint IN ( SELECT DISTINCT endpoint FROM log WHERE timestamp < ? );').bind(expiry).run();
 }
