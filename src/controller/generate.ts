d76bd430eef8ac0fa9a6aeaac32436a663f40a03
kj415j45 <919815238@qq.com>
2023-08-21 11:28:24 +0800

    Use correct collision detection.

--- src/controller/generate.ts
+++ src/controller/generate.ts
@@ -3,20 +3,19 @@ import exist from './exist';
 import pending from './pending';
 
 export default async function generate(env: Env, config?: { prefix?: string | null; suffix?: string | null }) {
+	const prefix = config?.prefix ?? '';
+	const suffix = config?.suffix ?? '';
+
 	let id: string;
 	let sf = snowflake();
 	do {
-		id = sf.next().value;
+		id = prefix + sf.next().value + suffix;
 		if (!(await exist(env, id))) {
 			break;
 		} else {
-			console.log(id + ' conflict');
+			console.log(`${id} conflict`);
 		}
 	} while (true);
-
-	const prefix = config?.prefix ?? '';
-	const suffix = config?.suffix ?? '';
-	id = prefix + id + suffix;
 
 	await pending(env, id);
 
