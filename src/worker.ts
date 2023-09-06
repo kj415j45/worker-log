5d9b7b08f25bc1379bfc2ee65e67baaa7ea2d99b
kj415j45 <919815238@qq.com>
2023-09-06 09:05:32 +0800

    Allow CORS.

--- src/worker.ts
+++ src/worker.ts
@@ -5,28 +5,34 @@ import log from './routes/log';
 
 export default {
 	async fetch(request: any, env: Env, ctx: ExecutionContext): Promise<Response> {
-		try {
-			const reqPath = new URL(request.url).pathname;
-
-			// dynamic route
-			if (reqPath.startsWith('/log/') && reqPath.length > 1) {
-				return log(request, env, ctx);
-			}
-
-			// static route
-			switch (reqPath) {
-				case '':
-				case '/':
-					return index(request, env, ctx);
-				default:
-					return error(request, env, ctx);
-			}
-		} catch (e) {
-			return error(request, env, ctx);
-		}
+		const res = await handle(request, env, ctx);
+		res.headers.set('Access-Control-Allow-Origin', '*');
+		return res;
 	},
 
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
 		ctx.waitUntil(cleanup(env));
 	},
 };
+
+async function handle(request: any, env: Env, ctx: ExecutionContext): Promise<Response> {
+	try {
+		const reqPath = new URL(request.url).pathname;
+
+		// dynamic route
+		if (reqPath.startsWith('/log/') && reqPath.length > 1) {
+			return log(request, env, ctx);
+		}
+
+		// static route
+		switch (reqPath) {
+			case '':
+			case '/':
+				return index(request, env, ctx);
+			default:
+				return error(request, env, ctx);
+		}
+	} catch (e) {
+		return error(request, env, ctx);
+	}
+}
