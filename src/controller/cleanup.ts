1fe58d8b5caee0f8d8cee0d216b28545072ec979
kj415j45 <919815238@qq.com>
2023-09-07 11:13:29 +0800

    Update incorrect func name.

--- src/controller/cleanup.ts
+++ src/controller/cleanup.ts
@@ -1,4 +1,4 @@
-export default async function exist(env: Env) {
+export default async function cleanup(env: Env) {
 	const expiry = new Date().setDate(new Date().getDate() - 1);
 	return env.DB.prepare('DELETE FROM log WHERE endpoint IN ( SELECT DISTINCT endpoint FROM log WHERE timestamp < ? );').bind(expiry).run();
 }
