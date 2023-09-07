2ec50b02792962fea383df0e6aec6e503fd6b89b
kj415j45 <919815238@qq.com>
2023-09-07 11:11:00 +0800

    Add index to improve (?) query performance

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
-CREATE TABLE IF NOT EXISTS log (endpoint TEXT, timestamp INTEGER, message TEXT)
+CREATE TABLE IF NOT EXISTS log (endpoint TEXT, timestamp INTEGER, message TEXT);
+CREATE INDEX IF NOT EXISTS idx_log_endpoint ON log (endpoint);
